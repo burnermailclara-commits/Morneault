166770bfd58a9b9617b0e4f838081ce69f7643ce
Clarisa <burnermailclara@gmail.com>
2026-05-10 14:24:17 -0600

    Fix white line above header: collapse menu wrapper div height

--- js/script.js
+++ js/script.js
@@ -59,6 +59,7 @@
 
     var wrapper = document.createElement('div');
     wrapper.innerHTML = html;
+    wrapper.style.cssText = 'height:0;line-height:0;font-size:0;overflow:visible;margin:0;padding:0;border:0';
     document.body.insertBefore(wrapper, document.body.firstChild);
 
     // Lógica del toggle
